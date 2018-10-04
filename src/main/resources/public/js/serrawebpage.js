51e3ca688d54af812bfcd83e7e9629ca6efb396c
federicoberti <federicoberti@192.168.1.3>
2018-10-04 22:40:08 +0200

    Aggiunta prima parte del client

---
+++ src/main/resources/public/js/serrawebpage.js
@@ -0,0 +1,20 @@
+var pageMod =(function(){
+
+
+  /**
+  *var
+  */
+  var prova ="";
+
+
+  function prova(){
+
+  }
+
+  return {
+    prova: function(){
+      console.log('DAJE');
+    },
+
+  };
+})();
